e0b3c45f7f512f56c3f9d2719c73aa656dffc4f1
Toromo A <toromo.ade06@gmail.com>
2022-04-19 14:08:37 -0500

    update

--- src/component/signup/CustomInput.jsx
+++ src/component/signup/CustomInput.jsx
@@ -1,73 +1,77 @@
-import { TextField } from "@mui/material";
-import React from "react";
-import { googleSignin } from "../../assets";
-import { Link } from "react-router-dom";
+import { TextField } from '@mui/material'
+import React from 'react'
+import { googleSignin } from '../../assets'
+import { Link } from 'react-router-dom'
 
 const CustomInput = ({ label }) => {
   return (
-    <div className="mb-6">
+    <div className='mb-6'>
       <TextField
         id={label}
         label={label}
-        variant="outlined"
-        size="small"
+        variant='outlined'
+        size='small'
         fullWidth
         InputLabelProps={{
           style: {
-            fontSize: "16px",
+            fontSize: '16px',
           },
         }}
       />
     </div>
-  );
-};
+  )
+}
 
-export default CustomInput;
+export default CustomInput
 
 export const CustomLogin = ({ login }) => {
   return (
     <>
-      <button className="flex items-center gap-2 border border-logoColor hover:bg-blue-300 rounded-[20px] pl-4 pr-11 py-1">
-        <img src={googleSignin} alt="Google" className="w-[28px] h-[28px] md:w-[38px] md:h-[38px] lg:w-[45px] lg:h-[45px]" />
-        <p className="text-logoColor text-phoneMobile md:text-signinlg lg:text-phoneMobileTwo font-semibold">
+      <button className='flex items-center gap-2 border border-logoColor hover:bg-blue-300 rounded-[20px] pl-4 pr-11 py-1'>
+        <img
+          src={googleSignin}
+          alt='Google'
+          className='w-[28px] h-[28px] md:w-[38px] md:h-[38px] lg:w-[45px] lg:h-[45px]'
+        />
+        <p className='text-logoColor text-phoneMobile md:text-signinlg lg:text-phoneMobileTwo font-semibold'>
           Sign In With Google
         </p>
       </button>
     </>
-  );
-};
+  )
+}
 
 export const CustomSupport = ({ support }) => {
   return (
     <>
-      <p className="text-footermd md:text-signinlg lg:text-phoneMobileTwo font-medium text-logoColor">
-        {support}{" "}
-        <Link to="" className="text-gold hover:text-logoColor">
+      <p className='text-footermd md:text-signinlg lg:text-phoneMobileTwo font-medium text-logoColor'>
+        {support}{' '}
+        <Link to='' className='text-gold hover:text-logoColor'>
           Contact Support
         </Link>
       </p>
     </>
-  );
-};
+  )
+}
 
 export const CustomButton = ({ buttonDetails }) => {
   return (
     <>
-      <button className="bg-logoColor font-bold text-white text-googleMobile md:text-signinlg lg:text-phoneMobileTwo hover:bg-gold hover:text-logoColor rounded-[20px] py-[7px] px-5">
+      <button className='bg-logoColor font-bold text-white text-googleMobile md:text-signinlg lg:text-phoneMobileTwo hover:bg-gold hover:text-logoColor rounded-[20px] py-[7px] px-5'>
         {buttonDetails}
       </button>
     </>
-  );
-};
+  )
+}
 
 export const CustomOr = () => {
   return (
     <>
-      <div className="w-full h-[1px] bg-orColor" />
-      <p className="font-semibold text-signinsm md:text-signinlg lg:text-phoneMobileTwo text-orColor border border-orColor rounded-[100%] px-[2px] py-[1px]">
+      <div className='w-full h-[1px] bg-orColor' />
+      <p className='font-semibold text-signinsm md:text-signinlg lg:text-phoneMobileTwo text-orColor border border-orColor rounded-[100%] px-[2px] py-[1px]'>
         OR
       </p>
-      <div className="w-full h-[1px] bg-orColor" />
+      <div className='w-full h-[1px] bg-orColor' />
     </>
-  );
-};
+  )
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,75 +1,75 @@
-import React from "react";
-import { logo, signupGoody } from "../assets";
-import { Link } from "react-router-dom";
+import { React, useState } from 'react'
+import { logo } from '../assets'
+import { Link } from 'react-router-dom'
 import CustomInput, {
   CustomButton,
   CustomLogin,
   CustomOr,
   CustomSupport,
-} from "../component/signup/CustomInput";
+} from '../component/signup/CustomInput'
 
 const Login = () => {
   return (
-    <div className="universal_padding h-screen md:flex md:justify-center md:items-center">
-      <div className="md:flex">
-        <div className="text-center mb-12 md:mb-0 pt-4 w-full">
-          <div className="md:mb-20 lg:mb-0">
+    <div className='universal_padding h-screen md:flex md:justify-center md:items-center'>
+      <div className='md:flex'>
+        <div className='text-center mb-12 md:mb-0 pt-4 w-full'>
+          <div className='md:mb-20 lg:mb-0'>
             <img
               src={logo}
-              alt="logo"
-              className="w-[100px] h-[52.57px] lg:w-[140px] lg:h-[80px] mx-auto md:mx-0 mb-2 lg:mb-4"
+              alt='logo'
+              className='w-[100px] h-[52.57px] lg:w-[140px] lg:h-[80px] mx-auto md:mx-0 mb-2 lg:mb-4'
             />
-            <h3 className="text-contriMobile md:text-contriDesktop lg:text-introH1Desktop text-logoColor font-semibold w-[60%] md:w-full mx-auto mb-2 md:text-left">
+            <h3 className='text-contriMobile md:text-contriDesktop lg:text-introH1Desktop text-logoColor font-semibold w-[60%] md:w-full mx-auto mb-2 md:text-left'>
               Pick up where you left off.
             </h3>
-            <p className="text-signupharsh font-semibold text-googleDesktop md:text-contriMobile md:text-left">
+            <p className='text-signupharsh font-semibold text-googleDesktop md:text-contriMobile md:text-left'>
               Sign in to continue.
             </p>
           </div>
         </div>
 
-        <div className="h-fit w-full">
-          <form className="signup_form px-4 md:py-2 mb-3">
+        <div className='h-fit w-full'>
+          <form className='signup_form px-4 md:py-2 mb-3'>
             {/* Custom input */}
-            <div className="mx-auto w-[80%] pt-14 mb-4">
-              <CustomInput label="Email Address" />
-              <CustomInput label="Password" />
-              <p className="-mt-6 text-signinsm text-gold font-semibold">
+            <div className='mx-auto w-[80%] pt-14 mb-4'>
+              <CustomInput label='Email Address' />
+              <CustomInput label='Password' />
+              <p className='-mt-6 text-signinsm text-gold font-semibold'>
                 Forgot password?
               </p>
             </div>
 
             {/* custom button*/}
-            <div className="flex justify-center items-center gap-2 mb-4">
-              <p className="text-signinsm font-medium text-logoColor">
-                Don’t have an account?{" "}
-                <Link to="/signup" className="text-gold">
+            <div className='flex justify-center items-center gap-2 mb-4'>
+              <p className='text-signinsm font-medium text-logoColor'>
+                Don’t have an account?{' '}
+                <Link to='/signup' className='text-gold'>
                   Create one
                 </Link>
               </p>
-              <CustomButton buttonDetails="Sign In" />
+              <CustomButton buttonDetails='Sign In' />
             </div>
 
-            <div className="text-center mb-4 md:mb-6">
-              <div className="flex justify-center items-center w-[87%] mx-auto gap-1">
+            <div className='text-center mb-4 md:mb-6'>
+              <div className='flex justify-center items-center w-[87%] mx-auto gap-1'>
                 <CustomOr />
               </div>
             </div>
 
             {/* custom Google Sign */}
-            <div className="flex justify-center mb-3 pb-6">
+            <div className='flex justify-center mb-3 pb-6'>
               <CustomLogin />
             </div>
           </form>
 
           {/* custom CustomerSupport */}
-          <div className="pb-3">
-            <CustomSupport support="Need help?" />
+          <div className='pb-3'>
+            <CustomSupport support='Need help?' />
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
