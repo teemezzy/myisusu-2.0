fe0f59d847107bcda093466483d27b0aba3f4654
Toromo A <toromo.ade06@gmail.com>
2022-05-16 13:52:22 -0500

    8th commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import React, { useEffect, useState, ajax } from 'react'
+// import axios from 'axios'
+import React, { useEffect, useState} from 'react'
 // import useFetch from '../hooks/useFetch'
 import { Link } from 'react-router-dom'
 import { logo, signupGoody } from '../assets'
